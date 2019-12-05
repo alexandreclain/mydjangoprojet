32b23dfc227cd53b4a2267fa69275046183ad77e
CLAIN Alexandre <aclain@cesi.fr>
2019-12-05 16:34:42 +0100

    next

--- monsite/settings.py
+++ monsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'scvr)ri5%5g%hgag#3%7%r-3+swj^v8n@p+#t5ln1c(x4hi0k&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'aclain.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'monsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['polls/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
